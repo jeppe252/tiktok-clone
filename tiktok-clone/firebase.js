b7ef3d480765f73eb8db2fcbf054a22834f9df42
jeppe252 <59533276+jeppe252@users.noreply.github.com>
2021-03-01 16:07:09 +0200

    first commit

---
+++ tiktok-clone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8FY8yT03KKKzZGW12EdfZfKJEkSyZJZA",
+    authDomain: "tiktok-clone-b8c9e.firebaseapp.com",
+    projectId: "tiktok-clone-b8c9e",
+    storageBucket: "tiktok-clone-b8c9e.appspot.com",
+    messagingSenderId: "806465455696",
+    appId: "1:806465455696:web:19565728c993ec1b9af355",
+    measurementId: "G-5E3MP91EFZ"
+  };
---
+++ tiktok-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Video from './Video';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Clever Programmers, Let's build the tiktok-clone</h1>
+
+    <div className="app__videos">
+      <video />
+      <video />
+      <video />
+      <video />
+    </div>
+    
+    </div>
+  );
+}
+
+export default App;
